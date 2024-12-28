e75c51a7428d5940792c9c249d473105f3682647
merynayr <boyarkin_dima2@mail.ru>
2024-12-28 15:05:30 +0300

    integrated service layer

--- internal/service/service.go
+++ internal/service/service.go
@@ -6,6 +6,7 @@ import (
 	"github.com/merynayr/auth/internal/model"
 )
 
+// UserService интерфейс сервисного слоя user
 type UserService interface {
 	GetUser(ctx context.Context, userID int64) (*model.User, error)
 	CreateUser(ctx context.Context, user *model.User) (int64, error)
--- internal/service/user/service.go
+++ internal/service/user/service.go
@@ -5,10 +5,13 @@ import (
 	"github.com/merynayr/auth/internal/service"
 )
 
+// Структура сервисного слоя с объектами репо слоя
+// и транзакционного менеджера
 type srv struct {
 	userRepository repository.UserRepository
 }
 
+// NewService возвращает объект сервисного слоя
 func NewService(userRepository repository.UserRepository) service.UserService {
 	return &srv{
 		userRepository: userRepository,
